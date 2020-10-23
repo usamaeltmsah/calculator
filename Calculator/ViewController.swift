c4c7c5f1f9a2208c8045087996aa272ab505df3c
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-23 02:07:36 +0200

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Usama Eltmsah on 10/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
