4fe49b88ed2ba3f3d0a656158970cbff8adf8f6d
usamaeltmsah <usamaeltmsah@gmail.com>
2020-11-01 00:25:02 +0200

    Calculate Sum of 2 numbers

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,6 +14,17 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var text_field1: UITextField!
+    @IBOutlet weak var text_field2: UITextField!
+    @IBOutlet weak var sum_butt: NSLayoutConstraint!
+    
+    @IBOutlet weak var sum_label: UILabel!
+    @IBAction func text_field2(_ sender: Any) {
+        let n1: Double = Double(text_field1.text!)!
+        let n2: Double = Double(text_field2.text!)!
+        
+        let sum = n1 + n2
+        sum_label.text = "Sum: \(sum)"
+    }
 }
 
